dd41b0437ee21e040d37fa6cefd5bcc51ecfd9f3
Git-zyj <yijie_zhi18@163.com>
2025-09-25 19:12:38 +0800

    add loop reverse for loop_transformation_classifier

--- loop_transformation_classifier/loop_transformation_classifier.py
+++ loop_transformation_classifier/loop_transformation_classifier.py
@@ -2,10 +2,10 @@ import re
 import os
 import numpy as np
 import tree_sitter as ts
+import tree_sitter_c as tsc
 import argparse as ap
 import pandas as pd
 import logging
-import tree_sitter_c as tsc
 from extraction_tools import extraction_tools
 
 class loop_transformation_classifiers:
@@ -23,25 +23,25 @@ class loop_transformation_classifiers:
         self.coefs = np.zeros([self.nstmts, self.d, self.d], dtype = int) # self.nstmts * var dim in dst * var dim in src
         self.var_neg = np.zeros([self.nstmts, self.d, self.d])    
 
-    def check_fusion_distribution(self, i, check_list, flags):
+    def check_fusion_distribution(self, i, check_list, tmp):
         '''
-        i: dimension\n
-        check_list: groups of statements (share the same loop)\n
-        flags: distinguish between fusion and distribution
+        i为维度,
+        check_list为语句簇(共用循环)
+        tmp为fusion, distribution判断
         '''
         
         
         if i >= np.shape(self.consts)[2]:
             # print("over")
-            return flags
+            return tmp
         
         # print(i)
         
         if set(self.consts[0, check_list, i]) == set(self.consts[1, check_list, i]) == {0}:
-            flags = self.check_fusion_distribution(i + 1, check_list, flags)
-            if flags == [1, 1]:
+            tmp = self.check_fusion_distribution(i + 1, check_list, tmp)
+            if tmp == [1, 1]:
                 # print("fusion + distribution")
-                return flags
+                return tmp
         else:
             co_loop = [[],[]]
 
@@ -54,12 +54,12 @@ class loop_transformation_classifiers:
             # print(co_loop[1])
 
             if len(co_loop[0]) < len(co_loop[1]):
-                flags[1] = 1
+                tmp[1] = 1
                 # print("distribution")
             elif len(co_loop[0]) > len(co_loop[1]):
-                flags[0] = 1
+                tmp[0] = 1
                 # print("fusion")
-            elif not co_loop[0] == co_loop[1] and set([str(x) for x in co_loop[0]]) != set([str(x) for x in co_loop[1]]):
+            elif co_loop[0] != co_loop[1] and set([str(x) for x in co_loop[0]]) != set([str(x) for x in co_loop[1]]):
                 # print("fusion + distribution")
                 return [1, 1]
             
@@ -69,12 +69,12 @@ class loop_transformation_classifiers:
                         new_check_list = list(set(group_src) & set(group_dst))
                         if len(new_check_list) - 1:
                             # print(f'new_check_list:{new_check_list}\n')
-                            flags = self.check_fusion_distribution(i + 1, new_check_list, flags) 
-                            if flags == [1, 1]:
+                            tmp = self.check_fusion_distribution(i + 1, new_check_list, tmp) 
+                            if tmp == [1, 1]:
                                 # print("fusion + distribution")
-                                return flags
+                                return tmp
                 
-        return flags
+        return tmp
 
     def check_nstmts_after(self):
         
@@ -96,33 +96,16 @@ class loop_transformation_classifiers:
         #     print(node.type, alias)
         return len(c_capture)
 
-    def check_tiling(self):
-        for i in range(self.nstmts):
-            for j in range(len(self.schedules[1][i])):
-                if "zT" in self.schedules[1][i][j]:                   
-                    self.answer[1] = 1 # "loop tiling"
-                    # print("tiling")
-                    return
-                    
-
     def loop_transformation_analysis(self):
         
-        self.check_tiling()
-              
-        self.loop_types[1] = [[element_type for element_schedule, element_type in zip(row_scheulde, row_type) if "zT" not in element_schedule] for row_scheulde, row_type in zip(self.schedules[1], self.loop_types[1])]      
-        self.schedules[1] = [[element for element in row if "zT" not in element] for row in self.schedules[1]]
-        
-        
-        # print(self.schedules[1])
-        # print(self.loop_types[1])
-        
-        min_length_schedule = min(len(row) for row in self.schedules[1])
-        
-        self.schedules[1] = [row[:min_length_schedule] for row in self.schedules[1]]
-        self.loop_types[1] = [row[:min_length_schedule] for row in self.loop_types[1]]
-        
-        # print(self.schedules[1])
-        # print(self.loop_types[1])
+        for i in range(self.nstmts):
+            for j in range(len(self.schedules[1][i])):
+                if "zT" in self.schedules[1][i][j]:
+                    self.schedules[1][i][j] = '0' # remove tiling dim
+                    self.loop_types[1][i][j] = 'scalar'
+                    
+                    self.answer[1] = 1 # "loop tiling"
+                    # print("tiling")
         
         for i in range(self.d): # for var dim in dst
 
@@ -140,14 +123,11 @@ class loop_transformation_classifiers:
                         self.loop_types[1][stmt].insert(2 * i, "scalar")
                 
                 if 2 * i + 1 < len(self.loop_types[1][j]):
-                    if self.loop_types[1][j][2 * i + 1] == 'scalar': 
-                        # scalar found in var dim in one stmt, check all
-                        if all([self.loop_types[1][stmt][2 * i + 1] == 'scalar' for stmt in range(self.nstmts)]): 
-                            # scalar found in var dim in all stmts, drop!
+                    if self.loop_types[1][j][2 * i + 1] == 'scalar':
+                        if all([self.loop_types[1][stmt][2 * i + 1] == 'scalar' for stmt in range(self.nstmts)]):
                             seq = 1
                             while 2 * i + 1 + seq < len(self.loop_types[1][j]):
                                 if any([self.loop_types[1][stmt][2 * i + 1 + seq] == 'loop' for stmt in range(self.nstmts)]):
-                                    # count scalar until meeting loop
                                     tmp_consts = dict()
                                     for stmt in range(self.nstmts):
                                         tmp_consts[stmt] = tuple(self.schedules[1][stmt][2 * i: 2 * i + 1 + seq])
@@ -159,7 +139,6 @@ class loop_transformation_classifiers:
                                     # print(sorted_consts)
                                     
                                     for stmt in range(self.nstmts):
-                                        # print(sorted_consts.index(tmp_consts[stmt]))
                                         self.schedules[1][stmt][2 * i] = str(sorted_consts.index(tmp_consts[stmt]))
                                         for _ in range(seq):
                                             del self.schedules[1][stmt][2 * i + 1]
@@ -177,16 +156,12 @@ class loop_transformation_classifiers:
                                 seq += 1
                     else:
                         for k in range(self.d): # for var dim in src
-                            if not self.schedules[0][j][2 * k + 1] == "0" and self.schedules[0][j][2 * k + 1] in self.schedules[1][j][2 * i + 1]: # find same iterator
+                            if not self.schedules[0][j][2 * k + 1] == "0" and self.schedules[0][j][2 * k + 1] in self.schedules[1][j][2 * i + 1]:
                                 self.coefs[j][i][k] = 1 # 后续考虑加上系数
-                            elif self.schedules[0][j][2 * k + 1][0] == "-" and self.schedules[0][j][2 * k + 1][1:] in self.schedules[1][j][2 * i + 1]: # find same negative iterator 
+                            elif self.schedules[0][j][2 * k + 1][0] == "-" and self.schedules[0][j][2 * k + 1][1:] in self.schedules[1][j][2 * i + 1]:
                                 self.coefs[j][i][k] = 1
                                 self.var_neg[j][i][k] = -1
-                                
-                # print(self.schedules[1][j])
-                # print(self.loop_types[1][j])    
 
-        # print(self.schedules)
         # print(len(self.loop_types[1][0]), len(self.loop_types[0][0]))
 
         if len(self.loop_types[1][0]) == len(self.loop_types[0][0]) - 1:
@@ -203,7 +178,6 @@ class loop_transformation_classifiers:
         # print(self.schedules)
         org_coefs = ~(self.schedules[0][:,1::2] == '0')
         # print(org_coefs)
-        
         for stmt in range(self.nstmts):
             for i in range(0, len(self.schedules[1][0]), 2):
                 if self.loop_types[1][stmt][i] != 'scalar':
@@ -218,7 +192,6 @@ class loop_transformation_classifiers:
                         self.loop_types[1][stmt][i + 1] = 'loop'
                         self.loop_types[1][stmt][i] = 'scalar'
                     else:
-                        # print(i, self.schedules[1][stmt], self.loop_types[1][stmt])
                         self.answer[8] = 1 # other transformation
                         return self.answer
         
@@ -238,9 +211,6 @@ class loop_transformation_classifiers:
         else:
             for i in range(self.nstmts):
                 if not (np.diag(self.coefs[i]) == org_coefs[i]).all():
-                    # print(i)
-                    # print(self.coefs[i])
-                    # print(org_coefs[i])
                     self.answer[2] = 1 # "loop interchange"
                     # print("interchange")
                     break
@@ -265,9 +235,9 @@ class loop_transformation_classifiers:
             # print('coefs:(var_dim_dst, self.nstmts, var_dim_src)\n', self.coefs)
             # print('consts:\n', self.consts)
 
-            flags = [0, 0]
+            tmp = [0, 0]
             check_list = [i for i in range(self.nstmts)]
-            self.answer[4:6] = self.check_fusion_distribution(0, check_list, flags) 
+            self.answer[4:6] = self.check_fusion_distribution(0, check_list, tmp) 
 
         
             
@@ -278,7 +248,7 @@ class loop_transformation_classifiers:
 if __name__ == "__main__":
     
     parser_description = '''
-    Distinguish types of loop transformations applied by pluto-0.11.4. Here are type could be identified now:
+    用于pluto-0.11.4所优化代码的循环变换种类判断，循环变换共分为:
     1. no loop transformation
     2. loop tiling
     3. loop intertchange
@@ -296,12 +266,43 @@ if __name__ == "__main__":
     parser.add_argument("-so", dest="suff_out", help="suffix of the file which contains stdout", type=str, default='.stdout')
     parser.add_argument("-sc", dest="suff_code", help="suffix of the file which contains code after transformation", type=str, default='.pluto.c')
     parser.add_argument("-o", dest="output", help="output of the classification answer", type=str, default='./classification_output.csv')
-    args = parser.parse_args()
+    ARGS = parser.parse_args()
+
+    # libs = ['numpy', 'tree_sitter', 'argparse', 'pandas']
+
+    # try:
+    #     import numpy as np
+    #     import tree_sitter as ts
+    #     import argparse as ap
+    #     import pandas as pd
+    #     print('Input library successful')
+    # except ModuleNotFoundError:
+    # 	for lib in libs:
+    #         print("Start install {0}".format(lib))
+    #         os.system('pip install %s'%(lib))
+    #         print('{0} install successful'.format(lib))
+    #         print('All install successful.\n')
+    #     import numpy as np
+    #     import tree_sitter as ts
+    #     import argparse as ap
+    #     import pandas as pd
+    #     print('Input library successful')
+
+    # if not os.path.exists("tree-sitter-c"):
+        
+    #     os.system("git clone https://github.com/tree-sitter/tree-sitter-c")
+    #     ts.Language.build_library(
+    #         # path to store .so file
+    #         './tree-sitter-c/build/my-languages.so',
+    #         [
+    #             './tree-sitter-c', # path of tree-sitter-c
+    #         ]
+    #     )
       
-    folder_stdout_path = args.folder_stdout_path # ./stdout
-    folder_code_path = args.folder_code_path # ./pluto_code
-    suffix_stdout = args.suff_out # '.stdout'
-    suffix_code = args.suff_code # '.pluto.c'
+    folder_stdout_path = ARGS.folder_stdout_path # ./stdout
+    folder_code_path = ARGS.folder_code_path # ./pluto_code
+    suffix_stdout = ARGS.suff_out # '.stdout'
+    suffix_code = ARGS.suff_code # '.pluto.c'
     
     log_path = 'exceptions.log'
     os.system(f'echo "">{log_path}')
@@ -324,20 +325,18 @@ if __name__ == "__main__":
             task = loop_transformation_classifiers(stmts, schedules, loop_types, c_codelet)
             answer = task.loop_transformation_analysis()
             answers.append([file_name] + answer)
-            # print("pass")
             
         except Exception as e:
             
             logging.basicConfig(filename=log_path, level=logging.ERROR)
             
             logging.exception('发生异常：')
-            # print("fail")
-            
+        
     data_columns = ['file_name', 'no loop transformation', 'loop tiling', 'loop interchange', \
         'loop skewing', 'loop fusion', 'loop distribution', 'loop reverse', 'loop shifting', \
         'other loop transformation']
     data = pd.DataFrame(answers, index = None, columns = data_columns)
-    data.to_csv(args.output)
+    data.to_csv(ARGS.output)
         
     
     
